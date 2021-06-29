94370f307598234cdaea03c9b5b82fef736b70ba
john lundgren <john.lundgren.lexicon@gmail.com>
2021-06-29 16:34:37 +0200

    Created store.

--- src/index.js
+++ src/index.js
@@ -3,42 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { createStore } from 'redux';
+import rootReducer from './reducers';
 
 //STORE -> GLOBALIZED STATE
-
-//ACTIONS -> AN ACTION IS A FUNCTION THAT RETURNS AN OBJECT
-const increment = () => {
-  return {
-    type: 'INCREMENT',
-  };
-};
-const decrement = () => {
-  return {
-    type: 'DECREMENT',
-  };
-};
-
-//REDUCER -> MODIFIES THE STORE UPON THE ACTION
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  }
-};
-
-let store = createStore(counter);
-
-//Display the store on the console
-store.subscribe(() => console.log(store.getState()));
-
-//DISPATCH
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(decrement());
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
