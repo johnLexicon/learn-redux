2c6a571d30986dde971272270b7b68254dcec30a
john lundgren <john.lundgren.lexicon@gmail.com>
2021-06-29 17:06:57 +0200

    Added code to activate devtools extension.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import { createStore } from 'redux';
 import rootReducer from './reducers';
 
 //STORE -> GLOBALIZED STATE
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
