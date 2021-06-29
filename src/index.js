92fcc6d14295532c2484f1af759d63f6f49fa593
john lundgren <john.lundgren.lexicon@gmail.com>
2021-06-29 15:53:06 +0200

    Created reducer.

--- src/index.js
+++ src/index.js
@@ -19,6 +19,16 @@ const decrement = () => {
 };
 
 //REDUCER -> MODIFIES THE STORE UPON THE ACTION
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
 
 //DISPATCH
 
